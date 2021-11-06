e9abc6dc1c0c0452383f3dbe2d25588610a3ceb8
ssrlive <30760636+ssrlive@users.noreply.github.com>
2021-11-06 11:59:55 +0800

    Update check.go

--- pkg/proxy/check.go
+++ pkg/proxy/check.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/ivpusic/grpool"
 
-	"github.com/Dreamacro/clash/adapters/outbound"
+	"github.com/Dreamacro/clash/adapter/outbound"
 )
 
 const defaultURLTestTimeout = time.Second * 5

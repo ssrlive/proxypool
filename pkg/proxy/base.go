db2b2e3adf897d0b9f8af274e09854e32653a9e4
ssrlive <30760636+ssrlive@users.noreply.github.com>
2021-12-07 17:52:23 +0800

    Update base.go

--- pkg/proxy/base.go
+++ pkg/proxy/base.go
@@ -96,7 +96,7 @@ func ParseProxyFromLink(link string) (p Proxy, err error) {
 	}
 	_, country, err := geoIp.GeoIpDB.Find(p.BaseInfo().Server) // IP库不准
 	if err != nil {
-		country = "�� ZZ"
+		country = "🏁 ZZ"
 	}
 	p.SetCountry(country)
 	// trojan依赖域名？<-这是啥?不管什么情况感觉都不应该替换域名为IP（主要是IP库的质量和节点质量不该挂钩）

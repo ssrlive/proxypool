3ea1e5367627175c64d3df438fb15221a9961ef8
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-07-23 13:33:41 +0800

    test code

---
+++ just_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ssrlive/proxypool/pkg/healthcheck"
+)
+
+func TestHello(t *testing.T) {
+	got := "Hello, world"
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got '%q' want '%q'", got, want)
+	}
+
+	healthcheck.PingFromChina("sina.com.cn", "443")
+}

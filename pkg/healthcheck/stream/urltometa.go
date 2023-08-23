4ae4eaf8e682f2a4e85d1018d367774618fad6da
asdlokj1qpi23 <138199492+asdlokj1qpi23@users.noreply.github.com>
2023-08-23 16:19:06 +0800

    Update urltometa.go

--- pkg/healthcheck/stream/urltometa.go
+++ pkg/healthcheck/stream/urltometa.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	C "github.com/Dreamacro/clash/constant"
 	"net/url"
+	"strconv"
 )
 
 func urlToMetadata(rawURL string) (addr C.Metadata, err error) {
@@ -27,7 +28,17 @@ func urlToMetadata(rawURL string) (addr C.Metadata, err error) {
 	addr = C.Metadata{
 		Host:    u.Hostname(),
 		DstIP:   nil,
-		DstPort: port,
+		DstPort: C.Port(convertPort(port)),
 	}
+	return
+}
+
+func convertPort(port string) (uint16num uint16) {
+	num, err := strconv.ParseUint(port, 10, 16)
+	if err != nil {
+		fmt.Println("转换失败：", err)
+		return
+	}
+	uint16num = uint16(num)
 	return
 }
